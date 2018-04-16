c42fe60c8ca47a114060ecb5e060b0a2939bf060
Mikaal Naik <matthewnaik@gmail.com>
2018-04-16 15:54:44 +0000

    Firt commit of html scripting thingy

---
+++ chunks.js
@@ -0,0 +1,22 @@
+var https = require('https');
+
+
+
+
+var options = {
+  host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+};
+
+// called by https when the request is made.
+var callback = function(response) {
+
+
+  response.on('data', function(chunk) {
+    console.log(chunk.toString() + '\n' );
+  });
+}
+
+
+
+https.request(options, callback).end();
