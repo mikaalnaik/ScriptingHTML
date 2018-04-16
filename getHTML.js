d52f9c91eef155b46ac6be871d4e0901d2e153a3
Mikaal Naik <matthewnaik@gmail.com>
2018-04-16 18:20:27 +0000

    Add callbacks

---
+++ getHTML.js
@@ -0,0 +1,41 @@
+var https = require('https');
+
+// function getHTML (options, callback) {
+
+  /* Add your code here */
+  var fetcher = function(response) {
+
+    response.on('data', function(chunk) {
+        var bufferedData = []
+          bufferedData.push(chunk.toString())
+
+        // for(var i = 0; i < bufferedData.length; i++) {
+          // console.log(bufferedData[i]);
+          printHTML(bufferedData)
+        // }
+      });
+  };
+
+  function getAndPrintHTML (targetHost, targetPath) {
+    /* Add your code here */
+       targetHost = requestOptions.host
+       targetPath = requestOptions.path
+  }
+
+// }
+
+function printHTML (html) {
+  for(var i = 0; i < html.length; i++)
+  console.log(html[i]);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+
+
+
+// getHTML(requestOptions, printHTML)
+https.request(requestOptions, fetcher).end();
